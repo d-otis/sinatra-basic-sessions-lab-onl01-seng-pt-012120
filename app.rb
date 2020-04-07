32441848fae89b6d0982030cdd163cd686cfae32
Dan Foley <dan@dan-foley.com>
2020-04-07 19:29:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   configure do
-    # enable :sessions
-    # set :session_secret, "|Pk=*H}zFe]-|+E]7Gchn5q8|~B9VDDBmsV@&g. z+z!B/|+/|P_#^)SwT}%WLzt"
+    enable :sessions
+    set :session_secret, "|Pk=*H}zFe]-|+E]7Gchn5q8|~B9VDDBmsV@&g. z+z!B/|+/|P_#^)SwT}%WLzt"
   end
 
   get '/' do
